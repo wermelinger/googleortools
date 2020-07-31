eb1857d706d2a26f59de3a0b65f3fcfbb681756a
Stefan Wermelinger <stefan.wermelinger@bbv.ch>
2020-07-31 14:06:09 +0200

    Prepared example 1 (simple)

--- GoogleOrToolsExamples/ConstraintOptimization1/CpSimple.cs
+++ GoogleOrToolsExamples/ConstraintOptimization1/CpSimple.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Google.OrTools.ConstraintSolver;
+﻿using ConstraintOptimization1;
+using Google.OrTools.Sat;
 
 namespace ConstraintOptimization
 {
@@ -7,33 +7,23 @@ namespace ConstraintOptimization
     {
         static void Main(string[] args)
         {
-            // Solver
-            Solver solver = new Solver("CpSimple");
+            // Define model
+            CpModel model = new CpModel();
 
-            // Variables
-            IntVar x = solver.MakeIntVar(0, 2, "x");
-            IntVar y = solver.MakeIntVar(0, 2, "y");
-            IntVar z = solver.MakeIntVar(0, 2, "z");
+            // Variables x,y,z with constraints: 0 - 2
+            IntVar x = model.NewIntVar(0, 2, "x");
+            IntVar y = model.NewIntVar(0, 2, "y");
+            IntVar z = model.NewIntVar(0, 2, "z");
 
-            // Constraint 0: x != y
-            solver.Add(solver.MakeAllDifferent(new IntVar[] { x, y }));
+            // Constraint: x != y
+            model.AddAllDifferent(new IntVar[] { x, y });
 
-            solver.Add(solver.MakeLess(y, z));
+            // Constraint: y < z
+            model.Add(y < z);
 
-            // Solve
-            DecisionBuilder db = solver.MakePhase(new IntVar[] { x, y, z }, Solver.CHOOSE_FIRST_UNBOUND, Solver.ASSIGN_MIN_VALUE);
-
-            // Print
-            int count = 0;
-            solver.NewSearch(db);
-            while(solver.NextSolution())
-            {
-                count++;
-                Console.WriteLine($"Solution: {count}\n x={x.Value()} y={y.Value()} z={z.Value()}");
-            }
-
-            solver.EndSearch();
-
+            // Solve: all feasible solutions
+            CpSolver solver = new CpSolver();
+            solver.SearchAllSolutions(model, new VarArraySolutionPrinter(new IntVar[] { x, y, z }));
         }
     }
 }
