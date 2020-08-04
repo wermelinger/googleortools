7f3c076b727bb536bb5dc7c6b4dd9fc39fc85779
Stefan Wermelinger <stefan.wermelinger@contractors.roche.com>
2020-08-04 18:21:32 +0200

    Revise ConstraintOptimizationSimple example for presentation.

---
+++ GoogleOrToolsExamples/ConstraintOptimizationSimple/CpSimple.cs
@@ -0,0 +1,29 @@
+﻿using Google.OrTools.Sat;
+
+namespace ConstraintOptimizationSimple
+{
+    class CpSimple
+    {
+        static void Main(string[] args)
+        {
+            // Define Model
+            var model = new CpModel();
+
+            // Variables x,y,z with constraints 0 - 2
+            var x = model.NewIntVar(0, 2, "x");
+            var y = model.NewIntVar(0, 2, "y");
+            var z = model.NewIntVar(0, 2, "z");
+
+            // Constraint: Variables different
+            model.AddAllDifferent(new [] { x, y, z });
+
+            // Constraint: Unique order -> one solution
+            model.Add(x < y);
+            model.Add(y < z);
+
+            // Solve: all feasible solutions
+            var solver = new CpSolver();
+            solver.SearchAllSolutions(model, new VarArraySolutionPrinter(new[] { x, y, z }));
+        }
+    }
+}
---
+++ GoogleOrToolsExamples/ConstraintOptimizationSimple/VarArraySolutionPrinter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Google.OrTools.Sat;
+
+namespace ConstraintOptimizationSimple
+{
+    public class VarArraySolutionPrinter : CpSolverSolutionCallback
+    {
+        private int solutionCount;
+        private IntVar[] variables;
+
+        public VarArraySolutionPrinter(IntVar[] variables)
+        {
+            this.variables = variables;
+        }
+
+        public override void OnSolutionCallback()
+        {
+            Console.WriteLine($"Solution #{solutionCount}: time = {WallTime():F2} s");
+            foreach (IntVar v in variables)
+            {
+                Console.WriteLine($"  {v.ShortString()} = {Value(v)}");
+            }
+            solutionCount++;
+        }
+    }
+}
